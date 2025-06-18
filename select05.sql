4a920f4e631e64a2e88f130bb1c845d065edc27a
oaihr <rhksgh0710@gmail.com>
2025-06-18 16:27:23 +0900

    복수행 함수(집계, 그룹화)

---
+++ select05.sql
@@ -0,0 +1,142 @@
+/******************************************************************/
+  복수행 함수 
+  
+  COUNT(대상) 갯수세기 
+  
+  select COUNT(*)
+  from emp;
+  
+  
+  --식별자 / 기본키(Primary Key)
+  select 
+    COUNT(empno),
+    COUNT(mgr),
+    COUNT(comm)    
+  from emp;
+  
+  select COUNT(*)
+  from emp
+  where comm is not null;
+  
+  select COUNT(*)
+  from student
+  where grade = 4;
+  
+  select COUNT(empno), SUM(sal)
+  from emp;
+  
+  --10,20부서에 총 몇 명? 총 급여 얼마씩?
+  select COUNT(empno), SUM(sal)
+  from emp
+  where deptno IN (10,20);
+  
+  select 
+    SUM(HEIGHT),    --합계
+    AVG(HEIGHT),    --평균
+    MAX(HEIGHT),    --최대
+    MIN(HEIGHT),    --최소
+    STDDEV(HEIGHT), --표준편차
+    VARIANCE(HEIGHT)--분산
+  from student;
+  
+  
+  
+  /******************************************************************/
+  GROUP BY 그룹화
+  그룹화 결과에 대해 조건 -> HAVING
+  
+  SELECT
+  FROM
+  WHERE
+  GROUP BY 컬럼대상 대상을 기준으로 그룹지어서 계산
+            GROUP BY에 명시한 컬럼은 SELECT 조회에 사용 가능
+  HAVING          
+  ORDER BY
+  
+  
+  
+  --각 학년별 평균키 
+  select '1학년' 학년, AVG(height) 평균키
+  from student
+  where grade = 1
+  UNION ALL
+  select '2학년',AVG(height)
+  from student
+  where grade = 2
+  UNION ALL
+  select '3학년',AVG(height)
+  from student
+  where grade = 3
+  UNION ALL
+  select '4학년',AVG(height)
+  from student
+  where grade = 4;
+  
+  
+  select grade, AVG(height)
+  from student
+  group by grade
+  order by grade desc;
+  
+  select studno, AVG(height)
+  from student
+  GROUP BY studno;
+  
+  select grade, AVG(height) avgh
+  from student
+  where grade IN (1,2,3)
+  group by grade
+  order by avgh;
+  --order by AVG(height) desc;
+  --order by grade desc;
+  
+  select deptno, AVG(sal)
+  from emp
+  group by deptno
+  having AVG(sal) >= 2000;
+  
+  1) 급여가 1600 이상인 직원들 대상, 부서별 평균 급여
+  select deptno, AVG(sal)
+  from emp
+  where sal >= 1600
+  group by deptno;
+  
+  2) 부서별 평균 급여가 1600 이상인 경우, 부서별 평균 급여
+  select deptno, AVG(sal)
+  from emp
+  group by deptno
+  having AVG(sal) >= 1600;
+  
+  
+  --
+  학생 각 학년별 평균 몸무게
+  평균 몸무게 65키로 이상
+  select grade, AVG(weight)
+  from student
+  group by grade
+  having AVG(weight) >= 65;
+  
+  
+  --
+  학생 4학년을 제외하고 각 학년별 평균 몸무게
+  평균 몸무게 65키로 이상
+  select grade, AVG(weight)
+  from student
+  where grade <> 4
+  group by grade
+  having AVG(weight) >= 65;
+  
+  
+  select deptno, job, AVG(sal), COUNT(*)
+  from emp
+  group by deptno, job;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
